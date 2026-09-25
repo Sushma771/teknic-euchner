f0a226c7181b543f7f77e12359ca08bbafc7f290
Sushma Deyannavar <sushma.r.deyannavar@gmail.com>
2026-09-25 10:56:48 +0530

    Prepare server for deployment

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const contactRoutes = require("./routes/contactRoutes");
 const errorHandler = require("./middleware/errorHandler");
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
@@ -41,6 +41,6 @@ app.use("/api/*splat", (req, res) => {
 app.use(errorHandler);
 
 // Start server
-app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
 });
